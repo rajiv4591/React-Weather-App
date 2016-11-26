2f14b5c9e34f3554cebb43b488f8b56dfe6b9858
Rajiv Ratan Reddy Dwarampudi <Rajiv Ratan Reddy Dwarampudi>
2016-11-26 15:04:11 -0500

    grid system and style examples

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
-        <h2>Examples Component!</h2>
+        <div>
+            <h1 className="text-center">Examples!</h1>
+            <p className="text-center">Here are a few example locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Charlotte'>Charlotte, NC</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
+        </div>
     );
 };
 
