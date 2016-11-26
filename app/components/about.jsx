e35af272b4e8a53ba27f65615ccf5c693fecdc77
Rajiv Ratan Reddy Dwarampudi <Rajiv Ratan Reddy Dwarampudi>
2016-11-26 15:17:20 -0500

    style about page and link to resources

--- app/components/about.jsx
+++ app/components/about.jsx
@@ -2,7 +2,19 @@ var React = require('react');
 
 var About = (props) => {
     return (
-        <h2>About Component</h2>
+        <div>
+            <h1 className="text-center">About</h1>
+            <p> This is a weather application built on React. It gives you the current temperature of the city you enter</p>
+            <p>Here are some tools that are used</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - This was the JavaScript framework used:
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> - This API was used to search for weather by city name.
+                </li>
+            </ul>
+        </div>
     );
 };
 
